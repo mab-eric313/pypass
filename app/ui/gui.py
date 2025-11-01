218435e797dd1b58647bc3efd4d64ccc46c95253
mab-eric313 <kseneric313@gmail.com>
2025-11-01 20:32:12 +0700

    Fix layout stack

--- app/ui/gui.py
+++ app/ui/gui.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from PySide6.QtCore import (
     Qt,
     QSize,
+    Signal,
 )
 
 from PySide6.QtGui import (
@@ -16,6 +17,7 @@ from PySide6.QtGui import (
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
+    QSizePolicy,
     QVBoxLayout,
     QHBoxLayout,
     QGridLayout,
@@ -44,59 +46,55 @@ class GuiApp:
             # Resolusi mobile
             # self.setMinimumSize(720, 1280)
 
-            layout_vbox = QVBoxLayout()
-            layout_content_hbox = QHBoxLayout()
-            self.layout_stack = QStackedLayout()
+            self.main_stack = GuiApp.MainStack()
+            self.bottom_bar = GuiApp.BottomBar()
 
-            layout_vbox.addLayout(self.layout_stack)
-            layout_vbox.addLayout(layout_content_hbox)
+            self.bottom_bar.switch_tab.connect(self.main_stack.set_tab)
 
-            button = QPushButton("Accounts")
-            button.pressed.connect(self.accounts_tab)
-            layout_content_hbox.addWidget(button)
-            self.layout_stack.addWidget(GuiApp.AccountsTab())
-
-            button = QPushButton("Settings")
-            button.pressed.connect(self.settings_tab)
-            layout_content_hbox.addWidget(button)
-            self.layout_stack.addWidget(GuiApp.SettingsTab())
+            layout = QVBoxLayout()
+            layout.addWidget(self.main_stack)
+            layout.addWidget(self.bottom_bar)
 
             container = QWidget()
-            container.setLayout(layout_vbox)
-
+            container.setLayout(layout)
             self.setCentralWidget(container)
 
-        def accounts_tab(self):
-            self.layout_stack.setCurrentIndex(0)
+    class MainStack(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.stack = QStackedLayout()
 
-        def settings_tab(self):
-            self.layout_stack.setCurrentIndex(1)
+            self.accounts_tab = GuiApp.AccountsTab()
+            self.settings_tab = GuiApp.SettingsTab()
+
+            self.stack.addWidget(self.accounts_tab)
+            self.stack.addWidget(self.settings_tab)
+
+            layout = QVBoxLayout()
+            layout.addLayout(self.stack)
+            self.setLayout(layout)
+
+        def set_tab(self, index: int):
+            self.stack.setCurrentIndex(index)
 
     class AccountsTab(QWidget):
         def __init__(self):
             super().__init__()
 
-            top_bar = GuiApp.TopBar()
-            content = GuiApp.Content()
-            # bottom_bar = GuiApp.BottomBar()
-
             layout = QVBoxLayout()
-            layout.addWidget(top_bar)
-            layout.addWidget(content)
-            # layout.addWidget(bottom_bar)
-
+            layout.addWidget(GuiApp.TopBar())
+            layout.addWidget(GuiApp.Content())
             self.setLayout(layout)
 
     class SettingsTab(QWidget):
         def __init__(self):
             super().__init__()
 
-            # bottom_bar = GuiApp.BottomBar()
-
             theme = QLabel("Theme:")
             system_default = QRadioButton("System default")
             light = QRadioButton("Light")
             dark = QRadioButton("Dark")
+            export_import = QPushButton("Export/Import")
 
             layout_vbox = QVBoxLayout()
             layout_theme = QHBoxLayout()
@@ -106,8 +104,9 @@ class GuiApp:
             layout_theme.addWidget(light)
             layout_theme.addWidget(dark)
 
+            layout_vbox.addWidget(export_import)
+
             layout_vbox.addLayout(layout_theme)
-            # layout_vbox.addWidget(bottom_bar)
 
             self.setLayout(layout_vbox)
 
@@ -116,8 +115,6 @@ class GuiApp:
             super().__init__()
 
             image_path = QPixmap(str(BASE_PATH / "assets" / "padlock.png"))
-            if image_path.isNull():
-                print("Error: Image top bar is not found")
 
             image = QLabel()
             image.setPixmap(image_path)
@@ -126,14 +123,12 @@ class GuiApp:
 
             search = QLineEdit()
             search.setPlaceholderText("Search")
-            search.setFixedSize(300, 40)
-
-            export_import = QPushButton("Export/Import")
+            search.setFixedHeight(30)
 
             layout = QHBoxLayout()
             layout.addWidget(image)
             layout.addWidget(search)
-            layout.addWidget(export_import)
+            layout.setSpacing(15)
 
             self.setLayout(layout)
 
@@ -141,7 +136,12 @@ class GuiApp:
         def __init__(self):
             super().__init__()
 
+            width = QSizePolicy.Expanding
+            height = QSizePolicy.Fixed
+
             button_add_password = QPushButton("Add password")
+            button_add_password.setSizePolicy(width, height)
+            button_add_password.setFixedHeight(60)
 
             # contoh
             button_github = QPushButton("Github")
@@ -167,49 +167,44 @@ class GuiApp:
             self.setLayout(layout_vbox)
 
     class BottomBar(QWidget):
+        switch_tab = Signal(int)
+
         def __init__(self):
             super().__init__()
 
-            image_button_path = QIcon(str(BASE_PATH / "assets" / "key.png"))
-            if image_button_path.isNull():
-                print("Error: Image button key is not found")
-
-            image_settings_path = QIcon(str(BASE_PATH / "assets" / "gear.png"))
-            if image_settings_path.isNull():
-                print("Error: Image settings key is not found")
+            icon_accounts = QIcon(str(BASE_PATH / "assets" / "key.png"))
+            icon_settings = QIcon(str(BASE_PATH / "assets" / "gear.png"))
 
             button_accounts = QPushButton()
-            button_accounts.setIcon(image_button_path)
+            button_accounts.setIcon(icon_accounts)
             button_accounts.setIconSize(QSize(24, 24))
             button_accounts.setFixedSize(72, 36)
 
-            label_accounts = QLabel("Accounts")
-            label_accounts.setAlignment(Qt.AlignHCenter)
+            label_accounts = QLabel("Accounts", alignment=Qt.AlignHCenter)
 
             button_settings = QPushButton()
-            button_settings.setIcon(image_settings_path)
+            button_settings.setIcon(icon_settings)
             button_settings.setIconSize(QSize(24, 24))
             button_settings.setFixedSize(72, 36)
 
-            label_settings = QLabel("Settings")
-            label_settings.setAlignment(Qt.AlignHCenter)
+            label_settings = QLabel("Settings", alignment=Qt.AlignHCenter)
 
             layout_button_accounts = QVBoxLayout()
             layout_button_accounts.addWidget(button_accounts)
             layout_button_accounts.addWidget(label_accounts)
-            layout_button_accounts.setAlignment(Qt.AlignHCenter)
 
             layout_button_settings = QVBoxLayout()
             layout_button_settings.addWidget(button_settings)
             layout_button_settings.addWidget(label_settings)
-            layout_button_accounts.setAlignment(Qt.AlignHCenter)
 
             layout = QHBoxLayout()
             layout.addLayout(layout_button_accounts)
             layout.addLayout(layout_button_settings)
             layout.setAlignment(Qt.AlignCenter)
-
             self.setLayout(layout)
+
+            button_accounts.clicked.connect(lambda: self.switch_tab.emit(0))
+            button_settings.clicked.connect(lambda: self.switch_tab.emit(1))
 
     def run(self):
         app = QApplication([])
