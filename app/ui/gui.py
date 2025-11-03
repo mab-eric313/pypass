a3496a096877fabc7ffd4bebb3fd0028ddf9dd67
mab-eric313 <kseneric313@gmail.com>
2025-11-03 07:11:23 +0700

    me-struktur ulang kode

--- app/ui/gui.py
+++ app/ui/gui.py
@@ -47,13 +47,13 @@ class GuiApp:
             # self.setMinimumSize(720, 1280)
             # self.setMinimumSize(360, 640)
 
-            self.main_stack = GuiApp.MainStack()
+            self.tab_stack = GuiApp.TabStack()
             self.bottom_bar = GuiApp.BottomBar()
 
-            self.bottom_bar.switch_tab.connect(self.main_stack.set_tab)
+            self.bottom_bar.switch_tab.connect(self.tab_stack.set_tab)
 
             layout = QVBoxLayout()
-            layout.addWidget(self.main_stack)
+            layout.addWidget(self.tab_stack)
             layout.addWidget(self.bottom_bar)
 
             container = QWidget()
@@ -61,7 +61,7 @@ class GuiApp:
 
             self.setCentralWidget(container)
 
-    class MainStack(QWidget):
+    class TabStack(QWidget):
         def __init__(self):
             super().__init__()
             self.stack = QStackedLayout()
@@ -76,47 +76,6 @@ class GuiApp:
 
         def set_tab(self, index: int):
             self.stack.setCurrentIndex(index)
-
-    class BottomBar(QWidget):
-        switch_tab = Signal(int)
-
-        def __init__(self):
-            super().__init__()
-
-            icon_accounts = QIcon(str(BASE_PATH / "assets" / "key.png"))
-            icon_settings = QIcon(str(BASE_PATH / "assets" / "gear.png"))
-
-            button_accounts = QPushButton()
-            button_accounts.setIcon(icon_accounts)
-            button_accounts.setIconSize(QSize(24, 24))
-            button_accounts.setFixedSize(72, 36)
-
-            label_accounts = QLabel("Accounts", alignment=Qt.AlignHCenter)
-
-            button_settings = QPushButton()
-            button_settings.setIcon(icon_settings)
-            button_settings.setIconSize(QSize(24, 24))
-            button_settings.setFixedSize(72, 36)
-
-            label_settings = QLabel("Settings", alignment=Qt.AlignHCenter)
-
-            layout_button_accounts = QVBoxLayout()
-            layout_button_accounts.addWidget(button_accounts)
-            layout_button_accounts.addWidget(label_accounts)
-
-            layout_button_settings = QVBoxLayout()
-            layout_button_settings.addWidget(button_settings)
-            layout_button_settings.addWidget(label_settings)
-
-            layout = QHBoxLayout()
-            layout.addLayout(layout_button_accounts)
-            layout.addLayout(layout_button_settings)
-            layout.setAlignment(Qt.AlignCenter)
-
-            self.setLayout(layout)
-
-            button_accounts.clicked.connect(lambda: self.switch_tab.emit(0))
-            button_settings.clicked.connect(lambda: self.switch_tab.emit(1))
 
     class TopBar(QWidget):
         def __init__(self):
@@ -140,37 +99,6 @@ class GuiApp:
 
             self.setLayout(layout)
 
-    class AccountsAddStack(QWidget):
-        switch_to_main = Signal()
-
-        def __init__(self):
-            super().__init__()
-
-            back = QPushButton("Back to Accounts")
-            back.clicked.connect(self.switch_to_main.emit)
-
-            layout = QVBoxLayout()
-            layout.addWidget(back)
-
-            self.setLayout(layout)
-
-        def set_tab(self, index: int):
-            self.stack.setCurrentIndex(index)
-
-    class AccountsAddContent(QWidget):
-        def __init__(self):
-            super().__init__()
-
-            back = QPushButton("This button from AccountsAddContent")
-
-            layout = QVBoxLayout()
-            layout.addWidget(back)
-
-            self.setLayout(layout)
-
-    class AccountsEditStack(QWidget):
-        pass
-
     class AccountsTab(QWidget):
         def __init__(self):
             super().__init__()
@@ -182,13 +110,13 @@ class GuiApp:
                 lambda: self.stack.setCurrentIndex(1)
             )
 
-            self.accounts_add_stack = GuiApp.AccountsAddStack()
-            self.accounts_add_stack.switch_to_main.connect(
+            self.accounts_add_content = GuiApp.AccountsAddContent()
+            self.accounts_add_content.switch_to_main.connect(
                 lambda: self.stack.setCurrentIndex(0)
             )
 
             self.stack.addWidget(self.accounts_content)
-            self.stack.addWidget(self.accounts_add_stack)
+            self.stack.addWidget(self.accounts_add_content)
 
             self.setLayout(self.stack)
 
@@ -230,6 +158,26 @@ class GuiApp:
             layout_vbox.addLayout(layout_grid)
             self.setLayout(layout_vbox)
 
+    class AccountsAddContent(QWidget):
+        switch_to_main = Signal()
+
+        def __init__(self):
+            super().__init__()
+
+            back = QPushButton("Back to Accounts")
+            back.clicked.connect(self.switch_to_main.emit)
+
+            layout = QVBoxLayout()
+            layout.addWidget(back)
+
+            self.setLayout(layout)
+
+        def set_tab(self, index: int):
+            self.stack.setCurrentIndex(index)
+
+    class AccountsEditContent(QWidget):
+        pass
+
     class SettingsTab(QWidget):
         def __init__(self):
             super().__init__()
@@ -261,6 +209,47 @@ class GuiApp:
             layout_vbox.addLayout(layout_theme)
 
             self.setLayout(layout_vbox)
+
+    class BottomBar(QWidget):
+        switch_tab = Signal(int)
+
+        def __init__(self):
+            super().__init__()
+
+            icon_accounts = QIcon(str(BASE_PATH / "assets" / "key.png"))
+            icon_settings = QIcon(str(BASE_PATH / "assets" / "gear.png"))
+
+            button_accounts = QPushButton()
+            button_accounts.setIcon(icon_accounts)
+            button_accounts.setIconSize(QSize(24, 24))
+            button_accounts.setFixedSize(72, 36)
+
+            label_accounts = QLabel("Accounts", alignment=Qt.AlignHCenter)
+
+            button_settings = QPushButton()
+            button_settings.setIcon(icon_settings)
+            button_settings.setIconSize(QSize(24, 24))
+            button_settings.setFixedSize(72, 36)
+
+            label_settings = QLabel("Settings", alignment=Qt.AlignHCenter)
+
+            layout_button_accounts = QVBoxLayout()
+            layout_button_accounts.addWidget(button_accounts)
+            layout_button_accounts.addWidget(label_accounts)
+
+            layout_button_settings = QVBoxLayout()
+            layout_button_settings.addWidget(button_settings)
+            layout_button_settings.addWidget(label_settings)
+
+            layout = QHBoxLayout()
+            layout.addLayout(layout_button_accounts)
+            layout.addLayout(layout_button_settings)
+            layout.setAlignment(Qt.AlignCenter)
+
+            self.setLayout(layout)
+
+            button_accounts.clicked.connect(lambda: self.switch_tab.emit(0))
+            button_settings.clicked.connect(lambda: self.switch_tab.emit(1))
 
     def run(self):
         app = QApplication([])
